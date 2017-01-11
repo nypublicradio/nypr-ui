c05fb39f5d024aeb85fcaba64b4defcd512cf311
Brian Whitton <me@brianwhitton.com>
2017-01-11 14:06:27 -0500

    adds nypr-mini-chrome component

---
+++ addon/components/nypr-mini-chrome.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+import layout from '../templates/components/nypr-mini-chrome';
+
+export default Ember.Component.extend({
+  classNames: ['nypr-mini-chrome'],
+  layout
+});
