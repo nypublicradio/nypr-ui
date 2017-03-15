683b75c8682a20b1998d0585d121884323f56674
Brian Whitton <me@brianwhitton.com>
2017-03-15 16:19:31 -0400

    clean up some imports

--- app/components/nypr-brick-card-row.js
+++ app/components/nypr-brick-card-row.js
@@ -1,7 +1,5 @@
 import Component from 'ember-component';
-import computed from 'ember-computed';
 
-export default Ember.Component.extend({
-  tagName: 'div',
+export default Component.extend({
   classNames: ['brick_row', 'brick_row--cards']
 });
