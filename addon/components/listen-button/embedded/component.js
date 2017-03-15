768edd72ea047acbe5c96f53fb6e6eb2908dadb8
Brian Whitton <me@brianwhitton.com>
2017-03-15 17:43:24 -0400

    adds listen button to nypr-ui

---
+++ addon/components/listen-button/embedded/component.js
@@ -0,0 +1,32 @@
+import ListenButton from '../component';
+import computed from 'ember-computed';
+import get from 'ember-metal/get';
+// import hbs from 'htmlbars-inline-precompile';
+import Ember from 'ember';
+
+function wnycEmbeddedAttr() {
+  return computed('embeddedAttrs', {
+    get(k) {
+      return get(this, `embeddedAttrs.${k}`);
+    },
+    set(k, v) {
+      return v;
+    }
+  });
+}
+
+export default ListenButton.extend({
+  itemPK:       wnycEmbeddedAttr(),
+  itemTitle:    wnycEmbeddedAttr(),
+  duration:     wnycEmbeddedAttr(),
+  playContext:  wnycEmbeddedAttr(),
+  type:         wnycEmbeddedAttr(),
+
+  state: computed('currentAudio', 'playState', 'itemPK', function() {
+    if (this.get('currentAudio') === this.get('itemPK')) {
+      return this.get('playState');
+    }
+  }),
+
+  layout: Ember.HTMLbars.compile(`{{listen-button.ui type=type}}{{#if (eq type 'blue-boss')}}Listen <span class="text--small dimmed">{{duration}}</span>{{/if}}`)
+});
