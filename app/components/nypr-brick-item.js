b2e4af8ae61c92d03dcbd9c15e33cc15d99a9563
Brian Whitton <me@brianwhitton.com>
2017-03-15 16:20:44 -0400

    use `item` instead of `items`

--- app/components/nypr-brick-item.js
+++ app/components/nypr-brick-item.js
@@ -1,9 +1,5 @@
 import Component from 'ember-component';
-import computed from 'ember-computed';
 
 export default Component.extend({
   tagName: '',
-  item: Ember.computed('items', function(){
-    return this.get('items');
-  }),
 });
