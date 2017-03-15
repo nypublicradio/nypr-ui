95f115c5bfc39ab9ee212115ea312e13708f61a4
Marine Boudeau <m14868@gmail.com>
2017-03-15 18:52:31 -0400

    Fix @noslouch's test

--- tests/integration/components/nypr-brick-grid-test.js
+++ tests/integration/components/nypr-brick-grid-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('nypr-brick-card-grid', 'Integration | Component | nypr brick card grid', {
+moduleForComponent('nypr-brick-grid', 'Integration | Component | nypr brick card grid', {
   integration: true
 });
 
@@ -10,15 +10,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{nypr-brick-card-grid}}`);
+  this.render(hbs`{{nypr-brick-grid}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#nypr-brick-card-grid}}
+    {{#nypr-brick-grid}}
       template block text
-    {{/nypr-brick-card-grid}}
+    {{/nypr-brick-grid}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
