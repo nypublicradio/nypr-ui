40f4415c1bd37586391f53a54a62f88cc18d0bc5
Marine Boudeau <m14868@gmail.com>
2017-03-22 11:22:36 -0400

    Conditionally show the additional image in the brick item component

--- app/components/nypr-brick-item.js
+++ app/components/nypr-brick-item.js
@@ -1,5 +1,10 @@
 import Component from 'ember-component';
+import computed from 'ember-computed';
 
 export default Component.extend({
   tagName: '',
+  vertical: computed(function() {
+    let template = this.get('template');
+    return template ? true : false;
+  })
 });
