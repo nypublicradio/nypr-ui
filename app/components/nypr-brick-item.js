e7225894c95329deaf4a2326b91b48d489ac9241
Marine Boudeau <m14868@gmail.com>
2017-03-22 11:22:36 -0400

    Conditionally show the additional image in the brick item component

--- app/components/nypr-brick-item.js
+++ app/components/nypr-brick-item.js
@@ -1,9 +1,11 @@
 import Component from 'ember-component';
 import computed from 'ember-computed';
+import { equals } from 'ember-computed';
 
 export default Component.extend({
   tagName: '',
   pk: computed('item', function() {
     return String(this.get('item.id'));
-  })
+  }),
+  vertical: Ember.computed.equal('template', 'vertical')
 });
