e1a33c20225df531ec518a708109091f638b2a28
Brian Whitton <me@brianwhitton.com>
2017-03-24 15:49:37 -0400

    stringify id as pk to match type in audio service

--- app/components/nypr-brick-item.js
+++ app/components/nypr-brick-item.js
@@ -1,5 +1,9 @@
 import Component from 'ember-component';
+import computed from 'ember-computed';
 
 export default Component.extend({
   tagName: '',
+  pk: computed('item', function() {
+    return String(this.get('item.id'));
+  })
 });
