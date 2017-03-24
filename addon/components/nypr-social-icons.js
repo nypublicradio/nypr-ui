66c648379314b592da3231f73d1d1e60a8fbd150
walsh9 <walsh9@gmail.com>
2017-03-24 12:26:25 -0400

    Add nypr-social-icons component

---
+++ addon/components/nypr-social-icons.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+import layout from '../templates/components/nypr-social-icons';
+
+export default Ember.Component.extend({
+  classNames: ["nypr-social-icons"],
+  layout
+});
