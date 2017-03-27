9bec8de5c29fd02a87db6bdb57d4668617c67a8b
Brian Whitton <me@brianwhitton.com>
2017-03-27 14:29:57 -0700

    `computed.equal` not `equals`

--- app/components/nypr-brick-item.js
+++ app/components/nypr-brick-item.js
@@ -1,12 +1,12 @@
 import Component from 'ember-component';
 import computed, { reads } from 'ember-computed';
-import { equals } from 'ember-computed';
+import { equal } from 'ember-computed';
 import htmlSafe from 'ember-string';
 
 export default Component.extend({
   tagName: '',
   pk: reads('item.id'),
-  vertical: equals('template', 'vertical'),
+  vertical: equal('template', 'vertical'),
   backgroundImage: computed('background-image', function() {
     var imageUrl = this.item.attributes.imageMain.url;
     return htmlSafe('background-image: url('+ imageUrl +')');
