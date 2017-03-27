b5677d236d07c3ac88995332015d56f2e7767b27
Brian Whitton <me@brianwhitton.com>
2017-03-27 13:56:46 -0700

    fix up brick item imports and syntax

--- app/components/nypr-brick-item.js
+++ app/components/nypr-brick-item.js
@@ -1,15 +1,16 @@
 import Component from 'ember-component';
 import computed from 'ember-computed';
 import { equals } from 'ember-computed';
+import htmlSafe from 'ember-string';
 
 export default Component.extend({
   tagName: '',
   pk: computed('item', function() {
     return String(this.get('item.id'));
   }),
-  vertical: Ember.computed.equal('template', 'vertical'),
-  backgroundImage: Ember.computed('background-image', function() {
+  vertical: equals('template', 'vertical'),
+  backgroundImage: computed('background-image', function() {
     var imageUrl = this.item.attributes.imageMain.url;
-    return Ember.String.htmlSafe('background-image: url('+ imageUrl +')');
+    return htmlSafe('background-image: url('+ imageUrl +')');
   })
 });
