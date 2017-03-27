3536a667f8ab8fb4cde215c2fd98de0a980b4c31
Brian Whitton <me@brianwhitton.com>
2017-03-27 13:59:09 -0700

    one more computed refactor

--- app/components/nypr-brick-item.js
+++ app/components/nypr-brick-item.js
@@ -1,13 +1,11 @@
 import Component from 'ember-component';
-import computed from 'ember-computed';
+import computed, { reads } from 'ember-computed';
 import { equals } from 'ember-computed';
 import htmlSafe from 'ember-string';
 
 export default Component.extend({
   tagName: '',
-  pk: computed('item', function() {
-    return String(this.get('item.id'));
-  }),
+  pk: reads('item.id'),
   vertical: equals('template', 'vertical'),
   backgroundImage: computed('background-image', function() {
     var imageUrl = this.item.attributes.imageMain.url;
