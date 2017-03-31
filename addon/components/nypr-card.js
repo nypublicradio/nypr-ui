bcf2aa7e6dccc5adc08937dc75f21b21c241d422
Brian Whitton <me@brianwhitton.com>
2017-03-31 12:45:45 -0400

    refactors nypr-card to yield neste components

--- addon/components/nypr-card.js
+++ addon/components/nypr-card.js
@@ -2,9 +2,6 @@ import Ember from 'ember';
 import layout from '../templates/components/nypr-card';
 
 export default Ember.Component.extend({
-  isEditing: false,
-
-  classNames: ['nypr-card'],
-
   layout,
+  classNames: ['nypr-card'],
 });
