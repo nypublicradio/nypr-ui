9a76f46f96772dc3cf5028f49ab1dfee97a1857d
Brian Whitton <me@brianwhitton.com>
2017-04-09 21:21:10 -0400

    this can be simpler now since it's just a container component

--- addon/components/nypr-brick-layout.js
+++ addon/components/nypr-brick-layout.js
@@ -1,16 +1,7 @@
 import Component from 'ember-component';
-import computed from 'ember-computed';
 import layout from '../templates/components/nypr-brick-layout';
 
 export default Component.extend({
   layout,
-  tagName: '',
-  groups: computed('items', function() {
-    let items = this.get('items');
-    return [
-      items.slice(0, 3),
-      items.slice(3, 5),
-      items.slice(5)
-    ];
-  })
+  tagName: ''
 });
