12fd4d869ea5a77d55cd036cf05b25b5fab4da67
Brian Whitton <me@brianwhitton.com>
2017-04-09 21:09:15 -0400

    add brick-row-blowout component

---
+++ addon/components/nypr-brick-row-blowout.js
@@ -0,0 +1,7 @@
+import Component from 'ember-component';
+import layout from '../templates/components/nypr-brick-row-blowout';
+
+export default Component.extend({
+  layout,
+  classNames: ['brick-row', 'brick-row--blowout']
+});
