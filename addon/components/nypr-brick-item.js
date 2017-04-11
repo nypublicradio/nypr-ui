8b84ae3e421f1f2d98eef6223db4b29251be6600
Diana Parrington <diana.parrington@gmail.com>
2017-04-11 11:15:19 -0400

    computed based on imageMain

--- addon/components/nypr-brick-item.js
+++ addon/components/nypr-brick-item.js
@@ -15,7 +15,7 @@ export default Component.extend({
   pk: reads('item.id'),
   vertical: equal('template', 'vertical'),
   backgroundImage: computed('item.attributes.imageMain.url', function() {
-    let imageMain = get(this, 'item.attributes.imageMain');
+    var imageMain = get(this, 'item.attributes.imageMain');
     var urlString;
     if (imageMain){
       // just in case we don't get a template and crop from the API, fallback to url
