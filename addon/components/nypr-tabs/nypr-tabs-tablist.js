fec6eb903627da9d5f51d8262203f59b2c990fde
Michael Hearn <git@mikehearn.net>
2017-04-18 11:44:18 -0400

    Extended ivy-tabs-tablist for design purposes

---
+++ addon/components/nypr-tabs/nypr-tabs-tablist.js
@@ -0,0 +1,6 @@
+import layout from '../../templates/components/nypr-tabs/nypr-tabs-tablist';
+import IvyTabsList from 'ivy-tabs/components/ivy-tabs-tablist';
+
+export default IvyTabsList.extend({
+  layout
+});
