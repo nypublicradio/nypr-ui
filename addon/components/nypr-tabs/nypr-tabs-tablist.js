5c38794fc4af97b9208d2f24c38315be6d57a70e
Michael Hearn <git@mikehearn.net>
2017-04-18 20:30:51 -0400

    Update ivy-tabs as regular dependency

--- addon/components/nypr-tabs/nypr-tabs-tablist.js
+++ addon/components/nypr-tabs/nypr-tabs-tablist.js
@@ -2,5 +2,6 @@ import layout from '../../templates/components/nypr-tabs/nypr-tabs-tablist';
 import IvyTabsList from 'ivy-tabs/components/ivy-tabs-tablist';
 
 export default IvyTabsList.extend({
-  layout
+  layout,
+  classNames: ['nypr-tabs-tablist']
 });
