ef0144b49e6ae5754c6dba5a74b709da91dabe2d
walsh9 <walsh9@gmail.com>
2017-05-17 14:26:08 -0400

    Add alert sub component to nypr-card

---
+++ addon/components/nypr-card/alert.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+import layout from '../../templates/components/nypr-card/alert';
+
+export default Ember.Component.extend({
+  layout,
+  classNames: ['nypr-card-alert'],
+});
