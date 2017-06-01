fcf25ab9fac165b829405cb70d088d79fdf1b2c6
Brian Whitton <me@brianwhitton.com>
2017-06-01 18:17:08 -0400

    adds nypr-floating-banner

---
+++ addon/components/nypr-floating-banner.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+import layout from '../templates/components/nypr-floating-banner';
+
+export default Ember.Component.extend({
+  layout,
+  tagName: '',
+
+  arrow: true
+});
