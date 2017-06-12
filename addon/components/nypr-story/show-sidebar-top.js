b965f36dd4a64e1c84be12fee7a5d7422060cbd2
Diana Parrington <diana.parrington@gmail.com>
2017-06-12 11:44:29 -0400

    show sidebar component, remove js style classes

---
+++ addon/components/nypr-story/show-sidebar-top.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+import layout from '../../templates/components/nypr-story/show-sidebar-top';
+
+export default Ember.Component.extend({
+  layout,
+  classNames: ['nypr-story-show-sidebar-top'],
+});
---
+++ app/components/nypr-story/show-sidebar-top.js
@@ -0,0 +1,1 @@
+export { default } from 'nypr-ui/components/nypr-story/show-sidebar-top';
