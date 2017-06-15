f8258e1f360e6439b0ad35f8547a39c03cd77a04
Brian Whitton <me@brianwhitton.com>
2017-06-15 13:44:58 -0400

    stories are identified by slug now
    don't need to coerce to string

--- addon/components/nypr-story/header-content.js
+++ addon/components/nypr-story/header-content.js
@@ -1,13 +1,7 @@
 import Ember from 'ember';
-import computed from 'ember-computed';
 import layout from '../../templates/components/nypr-story/header-content';
 import imageLoaderMixin from 'nypr-ui/mixins/image-loader';
 
 export default Ember.Component.extend(imageLoaderMixin, {
   layout,
-  pk: computed('story.id', function() {
-    // coerce to string b/c ember-data expects
-    // a string for IDs
-    return String(this.get('story.id'));
-  })
 });
--- addon/components/nypr-story/segment.js
+++ addon/components/nypr-story/segment.js
@@ -1,12 +1,6 @@
 import Ember from 'ember';
-import computed from 'ember-computed';
 import layout from '../../templates/components/nypr-story/segment';
 
 export default Ember.Component.extend({
   layout,
-  pk: computed('segment.id', function() {
-    // coerce to string b/c ember-data expects
-    // a string for IDs
-    return String(this.get('segment.id'));
-  }),
 });
