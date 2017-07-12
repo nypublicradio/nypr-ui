65240abace497c522a4f03a560572a432ac1612a
Brian Whitton <me@brianwhitton.com>
2017-07-12 19:36:09 -0400

    port show sidebar top dropdown to its own generic component

---
+++ addon/components/dropdown-panel.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+import layout from '../templates/components/dropdown-panel';
+
+export default Ember.Component.extend({
+  layout,
+  classNames: ['dropdown', 'dropdown--br', 'dropdown--nearwhite', 'dropdown-fade']
+});
