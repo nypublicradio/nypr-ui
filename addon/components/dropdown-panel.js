961b7c3468db5bf9a0f7f00a9ce30231b06239a3
Brian Whitton <me@brianwhitton.com>
2017-07-13 11:07:14 -0400

    dropdown panel refactor: allow consumer to specify color and direction

--- addon/components/dropdown-panel.js
+++ addon/components/dropdown-panel.js
@@ -4,5 +4,26 @@ import layout from '../templates/components/dropdown-panel';
 
 export default Component.extend({
   layout,
-  classNames: ['dropdown', 'dropdown--br', 'dropdown--nearwhite', 'dropdown-fade']
+  classNames: ['dropdown', 'dropdown-fade'],
+  classNameBindings: ['directionClass', 'colorClass'],
+  directionClass: computed('direction', function() {
+    let direction = this.get('direction');
+    switch(direction) {
+      case 'br':
+        return 'dropdown--br';
+      case 'tl':
+        return 'dropdown--tl';
+      default:
+        return 'dropdown--bl';
+    }
+  }),
+  colorClass: computed('color', function() {
+    let color = this.get('color');
+    switch (color) {
+      case 'white':
+        return 'dropdown--white';
+      default:
+        return 'dropdown--nearwhite';
+    }
+  })
 });
