add30e469d5f5c3575cd79c853d027989eb5243a
Brian Whitton <me@brianwhitton.com>
2017-07-13 11:06:34 -0400

    refactor dropdown panel imports

--- addon/components/dropdown-panel.js
+++ addon/components/dropdown-panel.js
@@ -1,7 +1,8 @@
-import Ember from 'ember';
+import computed from 'ember-computed';
+import Component from 'ember-component';
 import layout from '../templates/components/dropdown-panel';
 
-export default Ember.Component.extend({
+export default Component.extend({
   layout,
   classNames: ['dropdown', 'dropdown--br', 'dropdown--nearwhite', 'dropdown-fade']
 });
