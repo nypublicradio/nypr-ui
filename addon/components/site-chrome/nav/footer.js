ff7ecbb1a2c84facf38471ba535a3381cac55a83
Brian Whitton <me@brianwhitton.com>
2017-07-14 14:42:45 -0400

    adds site-chrome footer contextual component

---
+++ addon/components/site-chrome/nav/footer.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+import layout from '../../../templates/components/site-chrome/nav/footer';
+
+export default Ember.Component.extend({
+  layout,
+  classNames: ['site-chrome__nav__footer']
+});
