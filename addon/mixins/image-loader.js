2ea9501ad3ab37dde40bcab440ac1e795b6a4c14
Diana Parrington <diana.parrington@gmail.com>
2017-07-19 11:57:38 -0400

    add done fn to imagesLoaded

--- addon/mixins/image-loader.js
+++ addon/mixins/image-loader.js
@@ -2,10 +2,21 @@ import Ember from 'ember';
 
 export default Ember.Mixin.create({
   didInsertElement() {
-    this.$().imagesLoaded().progress((i, image) => {
-      Ember.run(() => {
-        image.img.classList.add('is-loaded');
+
+    this.$().imagesLoaded()
+      .progress((i, image) => {
+        Ember.run(() => {
+          image.img.classList.add('is-loaded');
+        });
+      })
+      .done((i) => {
+        if(Array.isArray(i.images) && i.images.length) {
+          i.images.forEach((image) => {
+            Ember.run(() => {
+              image.img.classList.add('is-loaded');
+            });
+          });
+        }
       });
-    });
-  }
+    }
 });
