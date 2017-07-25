1e72f07902aa903148d53603fd626f2f43c38efc
Brian Whitton <me@brianwhitton.com>
2017-07-25 12:43:09 -0400

    escape style tag on background div

--- addon/components/stream-banner.js
+++ addon/components/stream-banner.js
@@ -1,10 +1,14 @@
 import Component from 'ember-component';
 import layout from '../templates/components/stream-banner';
 import computed from 'ember-computed';
+import { htmlSafe } from 'ember-string';
 
 export default Component.extend({
   classNames: ['stream-banner'],
   layout,
   
-  activeStream: computed.reads('streams.firstObject')
+  activeStream: computed.reads('streams.firstObject'),
+  style: computed('background', function() {
+    return htmlSafe(`background-image: url(${this.get('background')});`);
+  })
 });
