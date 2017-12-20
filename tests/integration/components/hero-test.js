634404d532d1772224d395319874a1540669277c
Brian Whitton <me@brianwhitton.com>
2017-12-20 13:58:18 -0500

    adds basic rendering tests

--- tests/integration/components/hero-test.js
+++ tests/integration/components/hero-test.js
@@ -1,25 +1,50 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import { find } from 'ember-native-dom-helpers';
 
 moduleForComponent('nypr-ui/hero', 'Integration | Component | hero header', {
   integration: true
 });
 
 test('it renders', function(assert) {
-
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
   this.render(hbs`{{nypr-ui/hero}}`);
 
-  assert.equal(this.$().text().trim(), '');
+  assert.ok(find('.nypr-ui__hero'));
 
   // Template block usage:
   this.render(hbs`
-    {{#nypr-ui/hero}}
-      template block text
+    {{#nypr-ui/hero as |hero|}}
+      {{#hero.top as |top|}}
+        {{#top.left}}
+          Logo
+        {{/top.left}}
+        
+        {{#top.right}}
+          Donate
+        {{/top.right}}
+      {{/hero.top}}
+    
+      {{#hero.headline}}
+        Headline
+      {{/hero.headline}}
+      
+      {{#hero.blurb}}
+        Blurb
+      {{/hero.blurb}}
+      
+      {{#hero.lockup}}
+        lockup
+      {{/hero.lockup}}
     {{/nypr-ui/hero}}
   `);
+  
+  assert.ok(find('.hero-top'), 'top should render');
+  assert.ok(find('.hero-top__left'), 'top left should render');
+  assert.ok(find('.hero-top__right'), 'top right should render');
+  
+  assert.ok(find('.hero-headline'), 'headline');
+  assert.ok(find('.hero-blurb'), 'blurb');
+  assert.ok(find('.hero-lockup'), 'lockup');
+});
 
-  assert.equal(this.$().text().trim(), 'template block text');
 });
