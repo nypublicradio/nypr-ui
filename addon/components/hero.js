7e45ed1a6d800c1063b8afb54e52a7fb37517afc
Brian Whitton <me@brianwhitton.com>
2017-12-22 12:29:02 -0500

    WIP sticky header

--- addon/components/hero.js
+++ addon/components/hero.js
@@ -2,18 +2,49 @@ import Component from '@ember/component';
 import layout from '../templates/components/hero';
 import { htmlSafe } from '@ember/string';
 import { computed } from '@ember/object';
+import Waypoint from 'waypoints';
 
 export default Component.extend({
   layout,
   classNames: ['nypr-ui__hero'],
+  classNameBindings: ['isSticky'],
   attributeBindings: ['style'],
   
-  style: computed('src', function() {
+  didInsertElement() {
+    this._super(...arguments);
+    let waypoint = new Waypoint({
+      element: this.element,
+      handler: direction => {
+        this.set('isSticky', direction === 'down');
+      },
+      offset() {
+        return -this.element.clientHeight;
+      }
+    });
+    this.set('waypoint', waypoint);
+  },
+  
+  didRender() {
+    Waypoint.refreshAll();
+  },
+  
+  backgroundImage: computed('src', function() {
     let image = this.get('src');
     return htmlSafe(image ? `background-image: url(${image});` : '');
   }),
   
-  setImage(props) {
-    this.setProperties(props);
+  style: computed('isSticky', function() {
+    let height = this.element.clientHeight;
+    if (this.get('isSticky')) {
+      height = document.querySelector('.nypr-ui__hero > .nypr-ui__hero-body').clientHeight;
+      height += 100;
+      return htmlSafe(`height: ${height}px;`);
+    } else if (height === 0) {
+      return null;
+    }
+  }),
+  
+  setImage({ src, source, caption }) {
+    this.setProperties({ src, source, caption });
   }
 });
