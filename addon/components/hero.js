74777a98470f71c5d2f846b2e5df25c7f8af6751
Brian Whitton <me@brianwhitton.com>
2017-12-22 17:07:06 -0500

    move all this into a mixin and leverage the sticky shortcut

--- addon/components/hero.js
+++ addon/components/hero.js
@@ -2,46 +2,16 @@ import Component from '@ember/component';
 import layout from '../templates/components/hero';
 import { htmlSafe } from '@ember/string';
 import { computed } from '@ember/object';
-import Waypoint from 'waypoints';
+import StickyHeader from '../mixins/sticky-header';
 
-export default Component.extend({
+export default Component.extend(StickyHeader, {
   layout,
   classNames: ['nypr-ui__hero'],
-  classNameBindings: ['isSticky'],
   attributeBindings: ['style'],
   
-  didInsertElement() {
-    this._super(...arguments);
-    let waypoint = new Waypoint({
-      element: this.element,
-      handler: direction => {
-        this.set('isSticky', direction === 'down');
-      },
-      offset() {
-        return -this.element.clientHeight;
-      }
-    });
-    this.set('waypoint', waypoint);
-  },
-  
-  didRender() {
-    Waypoint.refreshAll();
-  },
-  
-  backgroundImage: computed('src', function() {
+  style: computed('src', function() {
     let image = this.get('src');
     return htmlSafe(image ? `background-image: url(${image});` : '');
-  }),
-  
-  style: computed('isSticky', function() {
-    let height = this.element.clientHeight;
-    if (this.get('isSticky')) {
-      height = document.querySelector('.nypr-ui__hero > .nypr-ui__hero-body').clientHeight;
-      height += 100;
-      return htmlSafe(`height: ${height}px;`);
-    } else if (height === 0) {
-      return null;
-    }
   }),
   
   setImage({ src, source, caption }) {
