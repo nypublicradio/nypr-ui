daac2765ff7bb54672711703687e09a0f8f22ef6
Brian Whitton <me@brianwhitton.com>
2017-12-26 09:25:06 -0500

    import fixes for waypoints
    there's no index file in the package, so we need to target a specific
    file in order to import into the build process.

--- index.js
+++ index.js
@@ -1,16 +1,54 @@
 /* eslint-env node */
 'use strict';
 
+var path = require('path');
+var Funnel = require('broccoli-funnel');
+var mergeTrees = require('broccoli-merge-trees');
+
 module.exports = {
   name: 'nypr-ui',
-  included: function(app) {
-    this._super.included(app);
-    
-    if (app.options.useWaypoints) {
-      app.import('node_modules/waypoints/lib/jquery.waypoints.js');
-      app.import('node_modules/waypoints/lib/shortcuts/sticky.js');
-      app.import('vendor/shims/waypoints.js');
+  included: function(app, parentAddon) {
+    this._super.included.apply(this, arguments);
+    if (!app.options.useWaypoints) {
+      return;
     }
+    var target = parentAddon || app;
+
+    while (target.app && !target.bowerDirectory) {
+      target = target.app;
+    }
+    
+    target.import({
+      development: 'vendor/third-party/jquery.waypoints.js',
+      production: 'vendor/third-party/jquery.waypoints.min.js',
+    });
+    
+    target.import({
+      development: 'vendor/third-party/sticky.js',
+      production: 'vendor/third-party/sticky.min.js'
+    });
+    
+    app.import('vendor/shims/waypoints.js');
+  },
+  treeForVendor(vendorTree) {
+    var waypointsTree = new Funnel(path.dirname(require.resolve('./node_modules/waypoints/lib/waypoints.debug.js')), {
+      files: [
+        'jquery.waypoints.js',
+        'jquery.waypoints.min.js',
+      ],
+      destDir: 'third-party'
+    });
+    
+    var stickyTree = new Funnel(path.dirname(require.resolve('./node_modules/waypoints/lib/shortcuts/sticky.js')), {
+      files: [
+        'sticky.js',
+        'sticky.min.js',
+      ],
+      destDir: 'third-party'
+    });
+    
+    return mergeTrees([vendorTree, waypointsTree, stickyTree]);
+
   },
   isDevelopingAddon: () => true
 };
