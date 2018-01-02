87bda2ae5c961bd3a7c0b0336d036246f5083e16
Brian Whitton <me@brianwhitton.com>
2018-01-02 12:42:44 -0500

    fade out headline and blurb whent hey reach the sticky header

--- addon/components/hero/blurb.js
+++ addon/components/hero/blurb.js
@@ -1,7 +1,24 @@
 import Component from '@ember/component';
 import layout from '../../templates/components/hero/blurb';
+import Waypoint from 'waypoints';
 
 export default Component.extend({
   layout,
-  classNames: ['hero-blurb']
+  classNames: ['hero-blurb'],
+  classNameBindings: ['fade:is-fade'],
+  
+  didInsertElement() {
+    this._super(...arguments);
+    let waypoint = new Waypoint({
+      element: this.element,
+      offset: 85,
+      handler: direction => this.set('fade', direction === 'down')
+    });
+    this.set('waypoint', waypoint);
+  },
+  
+  willDestroyElement() {
+    this._super(...arguments);
+    this.get('waypoint').destroy();
+  },
 });
