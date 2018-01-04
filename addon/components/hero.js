50e7519a70938abe23f263fcb869290ee81c73a9
Brian Whitton <me@brianwhitton.com>
2018-01-03 19:19:16 -0500

    watch a breakpoint to specify whether or not to use a background image
    these listeners always have a bit more ceremony than I'd prefer, but
    ignoring that, this adds a media query listner to watch a breakpoint,
    and if it matches, it uses a CSS background image for the hero image.
    
    otherwise it will use the image element specified in the hero/image.hbs
    template

--- addon/components/hero.js
+++ addon/components/hero.js
@@ -2,6 +2,7 @@ import Component from '@ember/component';
 import layout from '../templates/components/hero';
 import { htmlSafe } from '@ember/string';
 import { computed } from '@ember/object';
+import { bind } from '@ember/runloop';
 import StickyHeader from '../mixins/sticky-header';
 
 export default Component.extend(StickyHeader, {
@@ -9,10 +10,37 @@ export default Component.extend(StickyHeader, {
   classNames: ['nypr-ui__hero'],
   attributeBindings: ['style'],
   classNameBindings: ['gradient:with-gradient'],
+  breakpoint: '(min-width: 801px)',
   
-  style: computed('src', function() {
-    let image = this.get('src');
-    return htmlSafe(image ? `background-image: url(${image});` : '');
+  init() {
+    this._super(...arguments);
+    let breakpoint = this.get('breakpoint');
+    if (window.matchMedia &&  breakpoint) {
+      let mql = window.matchMedia(breakpoint);
+      this.set('mql', mql);
+      this.set('boundListener', bind(this, 'toggleImage'));
+      mql.addListener(this.get('boundListener'));
+      
+      this.toggleImage(mql);
+    }
+  },
+  
+  willDestroyElement() {
+    this._super(...arguments);
+    this.get('mql').removeListener(this.get('boundListener'));
+  },
+  
+  toggleImage({ matches }) {
+    this.set('useBackgroundImage', matches);
+  },
+  
+  style: computed('src', 'useBackgroundImage', function() {
+    if (this.get('useBackgroundImage'))  {
+      let image = this.get('src');
+      return htmlSafe(image ? `background-image: url(${image});` : '');
+    } else {
+      return htmlSafe('');
+    }
   }),
   
   setImage({ src, source, caption, credit }) {
