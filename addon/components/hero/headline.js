fc8c338e12f239060c9d6e0a8ac74894ba33e9ec
Brian Whitton <me@brianwhitton.com>
2018-01-04 11:57:08 -0500

    not going to fade out title and blurb anymore

--- addon/components/hero/headline.js
+++ addon/components/hero/headline.js
@@ -1,24 +1,7 @@
 import Component from '@ember/component';
 import layout from '../../templates/components/hero/headline';
-import Waypoint from 'waypoints';
 
 export default Component.extend({
   layout,
   classNames: ['hero-headline'],
-  classNameBindings: ['fade:is-fade'],
-  
-  didInsertElement() {
-    this._super(...arguments);
-    let waypoint = new Waypoint({
-      element: this.element,
-      offset: 85,
-      handler: direction => this.set('fade', direction === 'down')
-    });
-    this.set('waypoint', waypoint);
-  },
-  
-  willDestroyElement() {
-    this._super(...arguments);
-    this.get('waypoint').destroy();
-  },
 });
