4e1ba3215986c80085cae5312fb735d2515d641b
Brian Whitton <me@brianwhitton.com>
2018-01-05 18:11:11 -0500

    add srcset and sizes template helpers for images

---
+++ addon/helpers/image-srcset.js
@@ -0,0 +1,10 @@
+import { helper } from '@ember/component/helper';
+import { imageTemplate } from 'nypr-ui/helpers/image-template';
+
+export function imageSrcset([ image, ...sizes ] = ['', []]) {
+  let { template, crop } = image;
+  let srcset = sizes.map(([ w, h ]) => `${imageTemplate([template, w, h, crop])} ${w}w`);
+  return srcset.join(', ');
+}
+
+export default helper(imageSrcset);
