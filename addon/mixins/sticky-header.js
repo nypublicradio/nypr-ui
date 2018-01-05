cf3f4d8839d8efb16c744dc73461ac7e0371a6e3
Brian Whitton <me@brianwhitton.com>
2018-01-05 14:45:23 -0500

    make the sticky header optional at run time

--- addon/mixins/sticky-header.js
+++ addon/mixins/sticky-header.js
@@ -4,15 +4,19 @@ import Waypoint from 'waypoints';
 export default Mixin.create({
   didInsertElement() {
     this._super(...arguments);
-    let waypoint = new Waypoint.Sticky({
-      element: this.element,
-      offset: this.get('offset')
-    });
-    this.set('waypoint', waypoint);
+    if (this.get('sticky'))  {
+      let waypoint = new Waypoint.Sticky({
+        element: this.element,
+        offset: this.get('offset')
+      });
+      this.set('waypoint', waypoint);
+    }
   },
   
   willDestroyElement() {
     this._super(...arguments);
-    this.get('waypoint').destroy();
+    if (this.get('sticky'))  {
+      this.get('waypoint').destroy();
+    }
   },
 });
