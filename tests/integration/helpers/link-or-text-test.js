4a80b3a4cd6c354bdcf102c98e01f9a90c691f6d
walsh9 <walsh9@gmail.com>
2018-02-01 18:00:27 -0500

    Tweak image credits in nypr-hero

--- tests/integration/helpers/link-or-text-test.js
+++ tests/integration/helpers/link-or-text-test.js
@@ -6,12 +6,22 @@ moduleForComponent('link-or-text', 'helper:link-or-text', {
   integration: true
 });
 
-// Replace this with your real tests.
-test('it renders', function(assert) {
+test('it renders link', function(assert) {
+  let name = 'link';
+  let url = 'http://example.com';
+  this.set('inputValue', {name, url});
+
+  this.render(hbs`{{{link-or-text inputValue}}}`);
+
+  assert.equal(this.$('a').length, 1);
+  assert.equal(this.$('a').text(), name);
+  assert.equal(this.$('a').attr('href'), url);
+});
+
+test('it renders text', function(assert) {
   this.set('inputValue', '1234');
 
   this.render(hbs`{{link-or-text inputValue}}`);
 
   assert.equal(this.$().text().trim(), '1234');
 });
-
