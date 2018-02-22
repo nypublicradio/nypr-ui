61d59b5caaf76c7472de20b14eb6195ef3b6a83f
Brian Whitton <me@brianwhitton.com>
2018-02-22 12:42:24 -0500

    adds network stripe component

---
+++ addon/components/network-stripe.js
@@ -0,0 +1,7 @@
+import Component from '@ember/component';
+import layout from '../templates/components/network-stripe';
+
+export default Component.extend({
+  layout,
+  classNames: ['network-stripe']
+});
