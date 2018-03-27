9880c81a668e955406c64548607eff7498168361
Jeff Keen <jeff@keen.me>
2018-03-27 10:58:14 -0500

    Add network brand header with donate link and side menu to other sites

---
+++ tests/integration/components/nypr-brand-header-test.js
@@ -0,0 +1,20 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render, find, click, findAll } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | nypr-brand-header', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function(assert) {
+    await render(hbs`{{nypr-brand-header donationUrl="http://example.com/donate"}}`);
+    assert.equal(find('.mod-donate').getAttribute('href'), 'http://example.com/donate');
+  });
+
+  test('clicking nypr network opens menu', async function(assert) {
+    await render(hbs`{{nypr-brand-header donationUrl="http://example.com/donate"}}`);
+    click('.nypr-brand-header-button.mod-network')
+    assert.equal(findAll('.nypr-brand-header-menu-items li').length, 5, "menu should be visible");
+    click('.nypr-brand-header-button.mod-network') // reenable scroll
+  });
+});
