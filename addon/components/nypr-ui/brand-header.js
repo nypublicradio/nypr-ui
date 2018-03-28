733de250ded70625614b3e94d395a9cb47116b6d
Jeff Keen <jeff@keen.me>
2018-03-28 14:58:14 -0500

    Switch to burger menu since wnyc-studios was using that addon. We don't need two different side menu addons

--- addon/components/nypr-ui/brand-header.js
+++ addon/components/nypr-ui/brand-header.js
@@ -4,9 +4,24 @@ import { computed } from '@ember/object';
 export default Component.extend({
   layout,
   donationUrlWithRef: computed('donationUrl', function() {
-    let url = new URL(this.get('donationUrl'));
-    url.searchParams.set('ref', 'nypr-brand-header')
+    if (this.donationUrl) {
+      let url = new URL(this.donationUrl);
+      url.searchParams.set('ref', 'nypr-brand-header')
 
-    return url;
-  })
+      return url;
+    }
+  }),
+  tagName: '',
+  init() {
+    let menuItems = [
+      {name: "WNYC", url: "https://www.wnyc.org"},
+      {name: "Gothamist", url: "https://www.gothamist.com"},
+      {name: "WNYC Studios", url: "https://www.wnycstudios.org"},
+      {name: "WQXR", url: "https://www.wqxr.org"},
+      {name: "New Sounds", url: "https://www.newsounds.org"},
+    ];
+    this.set('menuItems', menuItems);
+
+    this._super(...arguments);
+  },
 });
