f138749d8481fcd22642d54d687afa0b25e7bb52
Brian Whitton <me@brianwhitton.com>
2018-03-30 12:10:24 -0400

    put background image on its own div for better responsive control

--- addon/components/support-stripe.js
+++ addon/components/support-stripe.js
@@ -7,9 +7,8 @@ export default Component.extend({
   layout,
   classNames: ['support-stripe'],
   classNameBindings: ['theme'],
-  attributeBindings: ['style'],
 
-  style: computed('background', function() {
+  image: computed('background', function() {
     let bg = this.background;
     if (bg) {
       return htmlSafe(`background-image: url(${bg});`);
