dd8ddbe505be92fa614da5ad24aed6efe9ea31ae
Brian Whitton <me@brianwhitton.com>
2018-04-26 14:45:00 -0400

    refactor nypr-share-button to a contextual component

---
+++ addon/components/nypr-share-button/button.js
@@ -0,0 +1,20 @@
+import Component from '@ember/component';
+import layout from '../../templates/components/nypr-share-button/button';
+
+export default Component.extend({
+  layout,
+  tagName: 'li',
+  classNames: ['nypr-sharebutton-listitem'],
+
+  closePopup() {}, // passed in from context
+
+  open(href) {
+    let { openPopup, closePopup, type } = this.getProperties('openPopup', 'closePopup', 'type');
+    if (openPopup) {
+      openPopup(type, href);
+    }
+    if (closePopup) {
+      closePopup();
+    }
+  }
+});
