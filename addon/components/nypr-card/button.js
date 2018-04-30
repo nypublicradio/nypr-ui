5c2f21fce4cf734b3b765436890fd7a7504b6d58
Brian Whitton <me@brianwhitton.com>
2018-04-30 13:09:36 -0400

    but this attribute does need to be bound for testing

--- addon/components/nypr-card/button.js
+++ addon/components/nypr-card/button.js
@@ -5,6 +5,6 @@ export default Component.extend({
   layout,
   tagName: 'button',
   classNames: ['nypr-card-button'],
-  attributeBindings: ['data-category', 'data-action', 'data-label', 'data-value'],
+  attributeBindings: ['data-category', 'data-action', 'data-label', 'data-value', 'data-test-selector'],
   'data-test-selector': 'nypr-card-button'
 });
