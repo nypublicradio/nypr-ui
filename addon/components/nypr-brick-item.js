ebd046fad78a5bc10262b14b917b36bdbfa1df48
Brian Whitton <me@brianwhitton.com>
2018-05-01 17:39:19 -0400

    not using the analytics attributes anymore
    add gtm analytics bindings

--- addon/components/nypr-brick-item.js
+++ addon/components/nypr-brick-item.js
@@ -10,7 +10,7 @@ export default Component.extend({
   layout,
   classNames: ['brick__item'],
   classNameBindings: ['item.attributes.template'],
-  attributeBindings: ['style'],
+  attributeBindings: ['style', 'data-category', 'data-action', 'data-label', 'data-value'],
 
   style: computed.reads('backgroundImage'),
   vertical: equal('template', 'vertical'),
@@ -29,16 +29,4 @@ export default Component.extend({
       return htmlSafe('');
     }
   }),
-  analyticsTitle: computed('item.attributes', function() {
-    let attributes = this.get('item.attributes');
-    if (!attributes) {
-      return;
-    }
-    let { headers, title } = attributes;
-    if (!headers || !headers.links) {
-      return title;
-    }
-    let shows = headers.links.reverse().map(l => l.title).join(' | ');
-    return shows += ` | ${title}`;
-  })
 });
