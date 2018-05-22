4703981b07944cb924c95951039ac4851db76a0a
Michael Hearn <git@mikehearn.net>
2018-05-22 10:37:31 -0400

    Basic logos displayed on desktop version

--- addon/components/nypr-ui/brand-header.js
+++ addon/components/nypr-ui/brand-header.js
@@ -5,15 +5,44 @@ export default Component.extend({
   tagName: '',
   init() {
     let menuItems = [
-      {name: "WNYC", url: "https://www.wnyc.org"},
-      {name: "Gothamist", url: "https://www.gothamist.com"},
-      {name: "WNYC Studios", url: "https://www.wnycstudios.org"},
-      {name: "NJPR", url: "http://www.njpublicradio.org"},
-      {name: "WQXR", url: "https://www.wqxr.org"},
-      {name: "New Sounds", url: "https://www.newsounds.org"}
+      {
+        name: 'WNYC',
+        url: 'https://www.wnyc.org',
+        svg: 'wnyc-logo',
+      },
+      {
+        name: 'Gothamist',
+        url: 'https://www.gothamist.com',
+        svg: 'gothamist-logo',
+      },
+      {
+        name: 'WNYC Studios',
+        url: 'https://www.wnycstudios.org',
+        svg: 'wnyc-studios-logo',
+      },
+      {
+        name: 'NJPR',
+        url: 'http://www.njpublicradio.org',
+        svg: 'njpr-logo',
+      },
+      {
+        name: 'WQXR',
+        url: 'https://www.wqxr.org',
+        svg: 'wqxr-logo',
+      },
+      {
+        name: 'New Sounds',
+        url: 'https://www.newsounds.org',
+        svg: 'new-sounds-logo',
+      },
+      {
+        name: 'The Greene Space',
+        url: 'https://www.thegreenespace.org',
+        svg: 'greene-space-logo',
+      },
     ];
     this.set('menuItems', menuItems);
 
     this._super(...arguments);
-  }
+  },
 });
