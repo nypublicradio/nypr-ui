6c0792544d41f7cd9686a4744bdd0378f2f3a293
Michael Hearn <git@mikehearn.net>
2018-05-23 11:00:09 -0400

    Add cookie onclick & check

--- addon/components/banner-with-close-button.js
+++ addon/components/banner-with-close-button.js
@@ -1,9 +1,23 @@
 import Component from '@ember/component';
 import layout from '../templates/components/banner-with-close-button';
+import {inject as service} from '@ember/service';
+import {computed} from '@ember/object';
 
 export default Component.extend({
   layout,
+  cookies: service(),
   classNames: ['banner-with-close-button'],
-  didInsertElement() {
-  }
+
+  isGdprCookieSet: computed('closed', function() {
+    let cookieService = this.get('cookies');
+    return cookieService.read('hasSeenGdprBanner');
+  }),
+
+  actions: {
+    setGdprCookie() {
+      let cookieService = this.get('cookies');
+      cookieService.write('hasSeenGdprBanner', true, {path: '/'});
+      this.set('closed', true);
+    },
+  },
 });
