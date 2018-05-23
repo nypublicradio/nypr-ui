7ddf4db6e965467484331b5ef3504dd016528a22
Michael Hearn <git@mikehearn.net>
2018-05-23 10:22:25 -0400

    Banner styled without cookies

---
+++ addon/components/banner-with-close-button.js
@@ -0,0 +1,9 @@
+import Component from '@ember/component';
+import layout from '../templates/components/banner-with-close-button';
+
+export default Component.extend({
+  layout,
+  classNames: ['banner-with-close-button'],
+  didInsertElement() {
+  }
+});
