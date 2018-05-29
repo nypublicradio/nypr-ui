0a9412efa82528abcd2abd7cc4eaf6a72a88aceb
Michael Hearn <git@mikehearn.net>
2018-05-29 15:55:38 -0400

    Add utm params to brand header links

--- addon/components/nypr-ui/brand-header.js
+++ addon/components/nypr-ui/brand-header.js
@@ -1,48 +1,52 @@
 import Component from '@ember/component';
 import layout from '../../templates/components/nypr-ui/brand-header';
+import { getOwner } from '@ember/application';
+
 export default Component.extend({
   layout,
   tagName: '',
   init() {
+    let config = getOwner(this).resolveRegistration('config:environment');
+
     let menuItems = [
       {
         name: 'WNYC',
-        url: 'https://www.wnyc.org',
+        url: `https://www.wnyc.org?utm_medium=partnersite&utm_campaign=brandheader&utm_source=${config.siteSlug}`,
         svg: 'wnyc-logo',
       },
       {
         name: 'Gothamist',
-        url: 'https://www.gothamist.com',
+        url: `https://www.gothamist.com?utm_medium=partnersite&utm_campaign=brandheader&utm_source=${config.siteSlug}`,
         svg: 'gothamist-logo',
       },
       {
         name: 'WNYC Studios',
-        url: 'https://www.wnycstudios.org',
+        url: `https://www.wnycstudios.org?utm_medium=partnersite&utm_campaign=brandheader&utm_source=${config.siteSlug}`,
         svg: 'wnyc-studios-logo',
       },
       {
         name: 'NJPR',
-        url: 'http://www.njpublicradio.org',
+        url: `http://www.njpublicradio.org?utm_medium=partnersite&utm_campaign=brandheader&utm_source=${config.siteSlug}`,
         svg: 'njpr-logo',
       },
       {
         name: 'WQXR',
-        url: 'https://www.wqxr.org',
+        url: `https://www.wqxr.org?utm_medium=partnersite&utm_campaign=brandheader&utm_source=${config.siteSlug}`,
         svg: 'wqxr-logo',
       },
       {
         name: 'New Sounds',
-        url: 'https://www.newsounds.org',
+        url: `https://www.newsounds.org?utm_medium=partnersite&utm_campaign=brandheader&utm_source=${config.siteSlug}`,
         svg: 'new-sounds-logo',
       },
       {
         name: 'The Greene Space',
-        url: 'https://www.thegreenespace.org',
+        url: `https://www.thegreenespace.org?utm_medium=partnersite&utm_campaign=brandheader&utm_source=${config.siteSlug}`,
         svg: 'greene-space-logo',
       },
     ];
     this.set('menuItems', menuItems);
 
     this._super(...arguments);
-  },
+  }
 });
