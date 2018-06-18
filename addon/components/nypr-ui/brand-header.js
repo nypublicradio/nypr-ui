71dd66cbff1ad87abb5c64cad6fd4872116d343e
Michael Hearn <git@mikehearn.net>
2018-06-17 21:39:40 -0400

    Update styles for RT-902

--- addon/components/nypr-ui/brand-header.js
+++ addon/components/nypr-ui/brand-header.js
@@ -11,25 +11,33 @@ export default Component.extend({
       {
         name: 'WNYC',
         slug: 'wnyc',
-        url: `https://www.wnyc.org`,
+        url: `https://www.wnyc.org?utm_medium=partnersite&utm_campaign=brandheader&utm_source=${this.get(
+          'siteSlug',
+        )}`,
         svg: 'wnyc-logo',
       },
       {
         name: 'Gothamist',
         slug: 'gothamist',
-        url: `https://www.gothamist.com`,
+        url: `https://www.gothamist.com?utm_medium=partnersite&utm_campaign=brandheader&utm_source=${this.get(
+          'siteSlug',
+        )}`,
         svg: 'gothamist-logo',
       },
       {
         name: 'WNYC Studios',
         slug: 'wnycstudios',
-        url: `https://www.wnycstudios.org`,
+        url: `https://www.wnycstudios.org?utm_medium=partnersite&utm_campaign=brandheader&utm_source=${this.get(
+          'siteSlug',
+        )}`,
         svg: 'wnyc-studios-logo',
       },
       {
         name: 'NJPR',
         slug: 'njpr',
-        url: `http://www.njpublicradio.org`,
+        url: `https://www.wqxr.org?utm_medium=partnersite&utm_campaign=brandheader&utm_source=${this.get(
+          'siteSlug',
+        )}`,
         svg: 'njpr-logo',
       },
       {
@@ -41,13 +49,17 @@ export default Component.extend({
       {
         name: 'New Sounds',
         slug: 'newsounds',
-        url: `https://www.newsounds.org`,
+        url: `https://www.newsounds.org?utm_medium=partnersite&utm_campaign=brandheader&utm_source=${this.get(
+          'siteSlug',
+        )}`,
         svg: 'new-sounds-logo',
       },
       {
         name: 'The Greene Space',
         slug: 'thegreenespace',
-        url: `https://www.thegreenespace.org`,
+        url: `https://www.thegreenespace.org?utm_medium=partnersite&utm_campaign=brandheader&utm_source=${this.get(
+          'siteSlug',
+        )}`,
         svg: 'greene-space-logo',
       },
     ];
