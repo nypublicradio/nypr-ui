9fd24a0f6eb7fd2fb8383da6906e0c613a74b691
Kate Edmundson <kedmundson@nypublicradio.org>
2018-06-27 13:10:39 -0400

    RT-724 lint fix

--- addon/components/logo-link.js
+++ addon/components/logo-link.js
@@ -1,7 +1,5 @@
 import Component from '@ember/component';
 import layout from '../templates/components/logo-link';
-import {getOwner} from '@ember/application';
-import {computed} from '@ember/object';
 
 export default Component.extend({
   layout,
